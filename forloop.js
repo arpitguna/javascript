876f653e49033c3de51e8500a2f54c8c019a4303
arpitguna <arpitguna150@gmail.com>
2024-08-16 13:41:35 +0530

    javascript

--- forloop.js
+++ forloop.js
@@ -1,9 +1,11 @@
 // 1 . . .
+console.log("-*-*-*-*-*-*-* 1 -*-*-*-*-*-*-*");
 for (let i = 1; i <= 10; i++) {
     console.log(i);
 }
 
 // 2 . . . 
+console.log("-*-*-*-*-*-*-* 2 -*-*-*-*-*-*-*");
 let sum = 0;
 for (let i = 1; i <= 10; i++) {
     sum = sum + i;
@@ -11,31 +13,43 @@ for (let i = 1; i <= 10; i++) {
 console.log(sum)
 
 // 3 . . . 
+console.log("-*-*-*-*-*-*-* 3 -*-*-*-*-*-*-*");
 for (let i = 1; i <= 5; i++) {
     console.log(i * i);
 }
 
 // 4 . . . 
+console.log("-*-*-*-*-*-*-* 4 -*-*-*-*-*-*-*");
 for (let i = 1; i <= 20; i = i + 2) {
     console.log(i)
 }
 
 // 5 . . .
+console.log("-*-*-*-*-*-*-* 5 -*-*-*-*-*-*-*");
 for (let i = 2; i <= 20; i = i + 2) {
     console.log(i)
 }
 
-//6 . . . 
-for (let i = 10; i >= 1; i--) {
+// 6 . . . 
+console.log("-*-*-*-*-*-*-* 6 -*-*-*-*-*-*-*");
+for(let i=1;i<=30;i++){
+    console.log("3 *",i,"=",3*i)
+}
 
+//7 . . . 
+console.log("-*-*-*-*-*-*-* 7 -*-*-*-*-*-*-*");
+for (let i = 10; i >= 1; i--) {
+    console.log(i)
 }
 
 //8 . . .
+console.log("-*-*-*-*-*-*-* 8 -*-*-*-*-*-*-*");
 for (let i = 1; i <= 5; i++) {
     console.log("Hello, World!")
 }
 
 //9 . . .
+console.log("-*-*-*-*-*-*-* 10 -*-*-*-*-*-*-*");
 let sum1 = 0;
 for (let i = 5; i <= 15; i++) {
     sum1 = sum1 + i;
