10d0b894dbd58fcec0685a38f93414b0a17e02f7
arpitguna <arpitguna150@gmail.com>
2024-08-16 13:24:30 +0530

    javascript

---
+++ forloop.js
@@ -0,0 +1,43 @@
+// 1 . . .
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// 2 . . . 
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum = sum + i;
+}
+console.log(sum)
+
+// 3 . . . 
+for (let i = 1; i <= 5; i++) {
+    console.log(i * i);
+}
+
+// 4 . . . 
+for (let i = 1; i <= 20; i = i + 2) {
+    console.log(i)
+}
+
+// 5 . . .
+for (let i = 2; i <= 20; i = i + 2) {
+    console.log(i)
+}
+
+//6 . . . 
+for (let i = 10; i >= 1; i--) {
+
+}
+
+//8 . . .
+for (let i = 1; i <= 5; i++) {
+    console.log("Hello, World!")
+}
+
+//9 . . .
+let sum1 = 0;
+for (let i = 5; i <= 15; i++) {
+    sum1 = sum1 + i;
+}
+console.log(sum1)
